e0a19bddb2febb6b33fd1c445e24349e55ae8d3c
DJWild2996 <d.j.wild2996@gmail.com>
2015-03-02 12:25:03 +1300

    V3

--- VB Leap Year Loop/Form1.vb
+++ VB Leap Year Loop/Form1.vb
@@ -12,21 +12,47 @@
     '        Next
     '    End Sub
 
+    'Private Sub btncheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncheck.Click
+
+    '    Dim year As Integer = txtyearenter.Text
+    '    Dim i As Integer = 1
+    '    'do while "i" is less than or equal to 10
+    '    Do While i <= 10
+    '        If year Mod 4 = 0 And (year Mod 100 <> 0 Or year Mod 400 = 0) Then
+    '            lbyear.Items.Add(year & " is a Leap Year")
+    '            i += 1
+    '        Else
+    '            'lbyear.items.add( year & " is not a leap year"
+    '        End If
+    '        year += 1
+    '    Loop
+
+    'End Sub
+
     Private Sub btncheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncheck.Click
+        Try
+            Dim year As Integer = txtyearenter.Text
 
-        Dim year As Integer = txtyearenter.Text
-        Dim i As Integer = 1
-        'do while "i" is less than or equal to 10
-        Do While i <= 10
-            If year Mod 4 = 0 And (year Mod 100 <> 0 Or year Mod 400 = 0) Then
-                lbyear.Items.Add(year & " is a Leap Year")
-                i += 1
-            Else
-                'lbyear.items.add( year & " is not a leap year"
-            End If
-            year += 1
-        Loop
+            Dim i As Integer = 1
+            Do While i <= 10
 
+                If year < 0 Then
+                    lbyear.Items.Add(year & " Invalid year... terminating!")
+                    Exit Do
+                End If
+
+                If year Mod 4 = 0 And (year Mod 100 <> 0 Or year Mod 400 = 0) Then
+                    lbyear.Items.Add(year & " is a leap year")
+                    i += 1
+                Else
+                    lbyear.Items.Add(year & " is not a leap year")
+                End If
+                year += 1
+            Loop
+        Catch
+            MsgBox("please put a number in")
+
+        End Try
     End Sub
 
 
