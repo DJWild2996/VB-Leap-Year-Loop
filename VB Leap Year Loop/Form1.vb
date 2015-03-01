112dc4ffae79ac220c50b967489632be25cbc1c4
DJWild2996 <d.j.wild2996@gmail.com>
2015-03-02 12:11:04 +1300

    V2

--- VB Leap Year Loop/Form1.vb
+++ VB Leap Year Loop/Form1.vb
@@ -1,15 +1,33 @@
 ﻿Public Class Form1
-    Private Sub btncheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncheck.Click
-        Dim year As Integer = txtyearenter.Text
+    '    Private Sub btncheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncheck.Click
+    '        Dim year As Integer = txtyearenter.Text
 
-        For i = 1 To 10
+    '        For i = 1 To 10
+    '            If year Mod 4 = 0 And (year Mod 100 <> 0 Or year Mod 400 = 0) Then
+    '                lbyear.Items.Add(year & " is a leap year")
+    '            Else
+    '                lbyear.Items.Add(year & " is not a leap year")
+    '            End If
+    '            year += 1
+    '        Next
+    '    End Sub
+
+    Private Sub btncheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncheck.Click
+
+        Dim year As Integer = txtyearenter.Text
+        Dim i As Integer = 1
+        'do while "i" is less than or equal to 10
+        Do While i <= 10
             If year Mod 4 = 0 And (year Mod 100 <> 0 Or year Mod 400 = 0) Then
-                lbyear.Items.Add(year & " is a leap year")
+                lbyear.Items.Add(year & " is a Leap Year")
+                i += 1
             Else
-                lbyear.Items.Add(year & " is not a leap year")
+                'lbyear.items.add( year & " is not a leap year"
             End If
             year += 1
-        Next
+        Loop
+
     End Sub
+
 
 End Class
